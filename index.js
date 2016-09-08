276c120a39f6b0faa5fdb22d8f68f75706705731
Leonid Fedotov <ileonidze@inbox.ru>
2016-09-09 01:43:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function arc4(string, key){
+(function(exports){
+    function arc4(string, key){
     var s = [], j = 0, x, res = '';
     for (var i = 0; i < 256; i++) {
         s[i] = i;
@@ -21,38 +22,38 @@ function arc4(string, key){
     }
     return res;
 }
-function randomString(size){
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for( var i=0; i < size; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
-}
-function encrypt(string,key,salt/*optional*/){
-    if(typeof salt !== "string" || !salt || salt==null || salt=="" || salt.length>9999){
-        var saltSize = 16;
-        if(string.length>64) saltSize = 32;
-        if(string.length>256) saltSize = 64;
-        if(string.length>1024) saltSize = 128;
-        salt = randomString(saltSize);
+    function randomString(size){
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for( var i=0; i < size; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
     }
-    var preEncodedString = "";
-    if(salt.length<1000) preEncodedString += "0";
-    if(salt.length<100) preEncodedString += "0";
-    if(salt.length<10) preEncodedString += "0";
-    preEncodedString += Number(salt.length).toString();
-    preEncodedString += salt;
-    preEncodedString += arc4(string,salt);
-    return arc4(preEncodedString,key);
-}
-function decrypt(string,key){
-    var preEncodedString = arc4(string,key);
-    var saltSize = parseInt(preEncodedString.substr(0,4));
-    var salt = preEncodedString.substr(4,saltSize);
-    return arc4(preEncodedString.substr(4+saltSize),salt);
-}
-
-module.exports = {
-    arc4:arc4,
-    encrypt:encrypt,
-    decrypt:decrypt
-};
+    function encrypt(string,key,salt/*optional*/){
+        if(typeof salt !== "string" || !salt || salt==null || salt=="" || salt.length>9999){
+            var saltSize = 16;
+            if(string.length>64) saltSize = 32;
+            if(string.length>256) saltSize = 64;
+            if(string.length>1024) saltSize = 128;
+            salt = randomString(saltSize);
+        }
+        var preEncodedString = "";
+        if(salt.length<1000) preEncodedString += "0";
+        if(salt.length<100) preEncodedString += "0";
+        if(salt.length<10) preEncodedString += "0";
+        preEncodedString += Number(salt.length).toString();
+        preEncodedString += salt;
+        preEncodedString += arc4(string,salt);
+        return arc4(preEncodedString,key);
+    }
+    function decrypt(string,key){
+        var preEncodedString = arc4(string,key);
+        var saltSize = parseInt(preEncodedString.substr(0,4));
+        var salt = preEncodedString.substr(4,saltSize);
+        return arc4(preEncodedString.substr(4+saltSize),salt);
+    }
+    module.exports = {
+        arc4:arc4,
+        encrypt:encrypt,
+        decrypt:decrypt
+    };
+})(typeof exports === 'undefined' ? this['rc4a']={} : exports);
